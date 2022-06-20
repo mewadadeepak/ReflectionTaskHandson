4c988c1dd4fc9d88503046a8dfb07a1f5f39ef0a
mewadadeepak <63440853+mewadadeepak@users.noreply.github.com>
2022-06-20 00:35:40 -0700

    Completed Reflection Task
    Competed Reflection Task

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionBugsFix
+{
+    //a custom attribute BugFix to be assigned to a class and its members
+    [AttributeUsage(
+       AttributeTargets.Class |
+       AttributeTargets.Constructor |
+       AttributeTargets.Field |
+       AttributeTargets.Method |
+       AttributeTargets.Property,
+       AllowMultiple = true)]
+
+    public class DeBugInfo : System.Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        public string message;
+
+        public DeBugInfo(int bg, string dev, string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+        public int BugNo
+        {
+            get
+            {
+                return bugNo;
+            }
+        }
+        public string Developer
+        {
+            get
+            {
+                return developer;
+            }
+        }
+        public string LastReview
+        {
+            get
+            {
+                return lastReview;
+            }
+        }
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+    [DeBugInfo(35,"Deepak Mewada", "18/06/222", Message = "semicolone not degined after body of for loop")]
+    [DeBugInfo(40, "Ankit Sainy", "10/10/2012", Message = "wrong syntex")]
+
+    class Rectangle
+    {
+        //member variables
+        protected double length;
+        protected double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        [DeBugInfo(55, "Deepak Mewada", "18/06/2022", Message = "Return type mismatch")]
+        public double GetArea()
+        {
+            return length * width;
+        }
+        [DeBugInfo(56, "Deepak Mewada", "18/06/2022")]
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(5.6, 9.5);
+            r.Display();
+            Type type = typeof(Rectangle);
+
+            //iterating through the attribtues of the Rectangle class
+            foreach (Object attributes in type.GetCustomAttributes(false))
+            {
+                DeBugInfo dbi = (DeBugInfo)attributes;
+
+                if (null != dbi)
+                {
+                    Console.WriteLine("Bug no: {0}", dbi.BugNo);
+                    Console.WriteLine("Developer: {0}", dbi.Developer);
+                    Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+                    Console.WriteLine("Remarks: {0}", dbi.Message);
+                }
+            }
+
+            //iterating through the method attribtues
+            foreach (MethodInfo m in type.GetMethods())
+            {
+
+                foreach (Attribute a in m.GetCustomAttributes(true))
+                {
+                    DeBugInfo dbi = (DeBugInfo)a;
+
+                    if (null != dbi)
+                    {
+                        Console.WriteLine("Bug no: {0}, for Method: {1}", dbi.BugNo, m.Name);
+                        Console.WriteLine("Developer: {0}", dbi.Developer);
+                        Console.WriteLine("Last Reviewed: {0}", dbi.LastReview);
+                        Console.WriteLine("Remarks: {0}", dbi.Message);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//internal class Program
